0c2f451c5d334c5997c7e128d29abc6dc22df525
farhansmb <41196166+farhansmb@users.noreply.github.com>
2022-03-27 06:45:27 +0530

    Add files via upload

---
+++ java/com/farhan/searchengine/config/StopWordsConfig.java
@@ -0,0 +1,7 @@
+package com.farhan.searchengine.config;
+
+
+public class StopWordsConfig {
+    public static final String STOP_WORD_FILE = "/home/toothless/Desktop/SearchEngine/src/main/resources/stopwords.txt";
+    public static final String STOP_WORD_DELIMITER=",";
+}
---
+++ java/com/farhan/searchengine/util/TimeUtil.java
@@ -0,0 +1,9 @@
+package com.farhan.searchengine.util;
+
+
+public class TimeUtil {
+
+    public static long getCurrentTimeInMs() {
+        return System.currentTimeMillis();
+    }
+}
